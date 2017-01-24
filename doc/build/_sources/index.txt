fbd2b8c3dbc8bf636917d377800af200c6237fec
מנחם סקלרץ <sklarz@auth.ad.bgu.ac.il>
2017-01-24 10:59:00 +0200

    added files

---
+++ doc/build/_sources/index.txt
@@ -0,0 +1,42 @@
+.. split_library_log_parser documentation master file, created by
+   sphinx-quickstart on Tue Jan 24 08:21:38 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to split_library_log_parser's documentation!
+====================================================
+
+Why should I need this?
+-------------------------
+
+When using QIIME, one of the first steps is the ``split_libraries_fastq.py`` script which *performs demultiplexing of Fastq sequence data where barcodes and sequences are contained in two separate fastq files*, while also doing some basic quality testing.
+
+This step creates a log file, *split_library_log.txt*, which contains statistics about the number of reads per sample that passed the various tests and manipulations.
+
+``parse_split_library_log.R`` converts this text file into a tsv table, which is more convenient for comparison between the samples etc. 
+
+Usage
+---------
+
+Simple! Just execute the following:::
+
+    Rscript parse_split_library_log.R   \
+        --input split_library_log.txt   \
+        --output split_library_log.tsv   
+    
+Where:
+
+--input file        Path to split_library_log.txt
+--output file       Path to tab-delimited output file
+
+Dependencies
+-------------
+
+Please make sure you have the following libraries installed in your R version:
+
+* magrittr
+* optparse
+* tools
+
+
+
---
+++ parse_split_library_log.R
@@ -0,0 +1,116 @@
+library(magrittr)
+library(optparse)
+library(tools)
+
+
+## Author: Menachem Sklarz
+## GITHUB repository: https://github.com/sklarz-bgu/split_library_log_parser.git
+## ReadsTheDocs: http://split-library-log-parser.readthedocs.io/en/latest/
+
+# 
+# paste("Rscript",
+#       "parse_split_library_log.R",
+#       "--input",    "split_library_log.txt") %>% system
+# paste("Rscript","parse_split_library_log.R","-h") %>% system
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+option_list = list(
+    make_option(c("-i", "--input"), type="character", default="split_library_log.txt", 
+                help="Path to split_library_log.txt", metavar="character"),
+    make_option(c("-o", "--output"), type="character", default=NULL, 
+                help="Path to tab-delimited output file", metavar="character")
+); 
+
+
+
+opt_parser = optparse::OptionParser(usage = "usage: %prog [options]", 
+                                    option_list=option_list,
+                                    epilogue="\n\nAuthor: Menachem Sklarz");
+opt = optparse::parse_args(opt_parser);
+
+
+if(!file.exists(opt$input)){
+    stop("The input file does not exist!")
+}
+
+if(!exists(x = "output",opt)) {
+    opt$output <- sprintf("%s.tsv",file_path_sans_ext(opt$input))
+    cat(sprintf("No output passed. Writing to %s",opt$output))
+}
+
+t1 <- readLines(opt$input)
+
+
+files <- grep(pattern = "Sequence read filepath",t1,value = T)  %>% 
+    strsplit(.," ")                                     %>% 
+    lapply(.,function(x) x[4])                          %>% 
+    unlist  %>% 
+    basename()
+
+total_seqs <- grep(pattern = "Total number seqs written",t1,value = T)  %>% 
+    strsplit(.,"\t")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist  %>%
+    as.numeric
+
+total_input <- grep(pattern = "Total number of input sequences",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist          %>%
+    as.numeric
+
+barcode <- grep(pattern = "Barcode not in mapping file",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+quality <- grep(pattern = "Read too short after quality truncation",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+Nchars <- grep(pattern = "Count of N characters exceeds limit",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+illumina_qual <- grep(pattern = "Illumina quality digit = 0",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+barcode_errors <- grep(pattern = "Barcode errors exceed max",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+Median_length <- grep(pattern = "Median sequence length",t1,value = T)  %>% 
+    strsplit(.,":")                                     %>% 
+    lapply(.,function(x) x[2])                          %>% 
+    unlist         %>%
+    as.numeric
+
+
+write.table(x = 
+                data.frame(sample=files, 
+                           total_input = total_input ,
+                           barcode = barcode ,
+                           quality = quality ,
+                           Nchars = Nchars ,
+                           illumina_qual = illumina_qual ,
+                           barcode_errors = barcode_errors ,
+                           Median_length = Median_length,
+                           total_seqs=total_seqs),
+            file = opt$output,
+            append = F,
+            quote = F,
+            sep = "\t",
+            row.names = F,
+            col.names = T)
